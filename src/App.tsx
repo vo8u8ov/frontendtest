9047a5569ec300a28ffd58e4922cf5ebba4c27a5
mariko.s <>
2024-10-25 23:33:52 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -26,10 +26,16 @@ const App: React.FC = () => {
     <div className="flex flex-col min-h-screen bg-black text-white">
       <Header />
       <div className="flex flex-grow p-4">
-        <div className="flex-grow">
-          <h2 className="text-lg font-bold">
-            データ表示エリア（チャートなど）
+        <div className="flex-grow p-4">
+          <h2 className="text-lg font-bold mb-1 flex items-center">
+            {/* アイコン */}
+            <span className="w-6 h-6 bg-gray-500 rounded mr-2"></span>
+            <span className="text-2xl">取引価格</span>
+            <span className="text-sm text-gray-400 ml-2 relative top-1">
+              ※取引面積1㎡あたり
+            </span>
           </h2>
+          <hr className="border-t border-gray-500 my-2" />
         </div>
         <UIPanel
           prefCode={prefCode}
