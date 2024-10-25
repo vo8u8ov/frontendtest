d928b48135dfd93eb49c79ce8d7e1ab0d571cc16
mariko.s <>
2024-10-25 23:35:14 +0900

    wip

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,7 @@ const Footer: React.FC = () => {
     <footer className="bg-gray-800 p-4 text-left flex justify-between items-center">
       <div className="flex items-center">
         <a href="/terms" className="flex items-center mr-4">
+          利用規約
           <svg
             className="h-5 w-5 mr-1 text-white"
             xmlns="http://www.w3.org/2000/svg"
@@ -20,9 +21,9 @@ const Footer: React.FC = () => {
               d="M12 8v8m0 0v4m0-4H8m4 0h4"
             />
           </svg>
-          利用規約
         </a>
         <a href="/privacy" className="flex items-center">
+          プライバシーポリシー
           <svg
             className="h-5 w-5 mr-1 text-white"
             xmlns="http://www.w3.org/2000/svg"
@@ -37,7 +38,6 @@ const Footer: React.FC = () => {
               d="M12 8v8m0 0v4m0-4H8m4 0h4"
             />
           </svg>
-          プライバシーポリシー
         </a>
       </div>
       <p>&copy; 2023 Landit Inc.</p> {/* ここを変更 */}
