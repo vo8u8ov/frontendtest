1e0ec01ddaa83bf52f162b9ce93a80175a4100cc
mariko.s <>
2024-10-27 23:35:32 +0900

    firebaseのapiキー公開（問題なさそう?)

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDaCSNH86fj-91A9-IlQ8cBA2H2GP01YvM",
+  authDomain: "codingtask-bde07.firebaseapp.com",
+  databaseURL:
+    "https://codingtask-bde07-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "codingtask-bde07",
+  storageBucket: "codingtask-bde07.appspot.com",
+  messagingSenderId: "754819618462",
+  appId: "1:754819618462:web:79d3c969046814d9d08d8f",
+  measurementId: "G-HB924PHSZN",
 };
 
 const app = initializeApp(firebaseConfig);
