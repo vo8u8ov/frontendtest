11ac0aa62fed4e0902366df3de4811c7d61bddb7
mariko.s <>
2024-10-28 15:20:13 +0900

    UIパネルのアイコン変更

--- src/components/UIPanel.tsx
+++ src/components/UIPanel.tsx
@@ -2,6 +2,9 @@
 import React from "react";
 import { years } from "../years";
 import { prefectures } from "../prefectures";
+import MapIcon from "../marker.svg";
+import CalendarIcon from "../calendar-check.svg";
+import TypeIcon from "../resources.svg";
 
 interface UIPanelProps {
   prefCode: number;
@@ -12,54 +15,6 @@ interface UIPanelProps {
   handleYearChange: (year: number) => void;
   handleDisplayTypeChange: (type: number) => void;
 }
-
-const MapIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    fill="none"
-    viewBox="0 0 24 24"
-    className="mr-2 text-gray-300"
-  >
-    <path
-      fill="currentColor"
-      d="M12 2C10.343 2 9 3.343 9 5c0 .182.018.357.052.529C6.353 6.434 4 8.716 4 11.5 4 14.365 7.582 17 12 17s8-2.635 8-5.5c0-2.784-2.353-5.066-5.052-5.971C12.982 2.357 12 2 12 2z"
-    />
-  </svg>
-);
-
-const CalendarIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    fill="none"
-    viewBox="0 0 24 24"
-    className="mr-2 text-gray-300"
-  >
-    <path
-      fill="currentColor"
-      d="M19 3h-2V1h-2v2H7V1H5v2H1v16h22V3h-4zm0 18H5V8h14v13z"
-    />
-  </svg>
-);
-
-const TypeIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    fill="none"
-    viewBox="0 0 24 24"
-    className="mr-2 text-gray-300"
-  >
-    <path
-      fill="currentColor"
-      d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm0-4h-2V7h2v8z"
-    />
-  </svg>
-);
 
 const UIPanel: React.FC<UIPanelProps> = ({
   prefCode,
@@ -86,7 +41,13 @@ const UIPanel: React.FC<UIPanelProps> = ({
 
       {/* 場所 */}
       <div className="flex items-center mb-4">
-        <MapIcon />
+        <img
+          src={MapIcon}
+          alt="Map Icon"
+          width="20"
+          height="20"
+          className="mr-2 text-gray-300"
+        />
         <label className="w-1/3 mr-2">場所</label>
         <select
           value={prefCode}
@@ -103,7 +64,13 @@ const UIPanel: React.FC<UIPanelProps> = ({
 
       {/* 年度 */}
       <div className="flex items-center mb-4">
-        <CalendarIcon />
+        <img
+          src={CalendarIcon}
+          alt="Calendar Icon"
+          width="20"
+          height="20"
+          className="mr-2 text-gray-300"
+        />
         <label className="w-1/3 mr-2">年度</label>
         <select
           value={selectedYear}
@@ -120,7 +87,13 @@ const UIPanel: React.FC<UIPanelProps> = ({
 
       {/* 表示タイプ */}
       <div className="flex items-start mb-4">
-        <TypeIcon />
+        <img
+          src={TypeIcon}
+          alt="Type Icon"
+          width="20"
+          height="20"
+          className="mr-2 text-gray-300"
+        />
         <label className="w-1/3 mr-2">種類</label>
         <div className="flex flex-col w-2/3">
           <label className="flex items-center mb-1">
