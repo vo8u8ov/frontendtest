ce38154c4ad79195c85980ed73f1c6e420f985fb
mariko.s <>
2024-10-29 15:30:57 +0900

    ヘッダーのレイアウト修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,17 +5,19 @@ import Logo from "../logo-landit.svg";
 const Header: React.FC = () => {
   return (
     <header
-      className="bg-gray-800 flex items-center gap-2"
+      className="bg-gray-800 flex items-center gap-10"
       style={{
         height: "74px",
-        width: "1920px",
-        paddingRight: "24px",
-        paddingLeft: "2px",
+        width: "100%",
+        padding: "0px 24px 0px 2px",
         backgroundColor: "#FFFFFF",
-        gap: "10px",
       }}
     >
-      <img src={Logo} alt="Landit Logo" style={{ width: "233px", height: "60px" }} />
+      <img
+        src={Logo}
+        alt="Landit Logo"
+        style={{ width: "233px", height: "60px" }}
+      />
     </header>
   );
 };
