2d068f624f302ce440ad6a4f5a5291831b06b5c0
mariko.s <>
2024-10-29 15:30:44 +0900

    画面全体のレイアウト調整

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ import DataDisplayArea from "./components/DataDisplayArea";
 
 const App: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen text-white">
+    <div
+      style={{
+        width: "1920px", // プロジェクト全体の固定幅
+        height: "1080px", // プロジェクト全体の固定高さ
+        display: "flex", // フレックスボックスを使用
+        flexDirection: "column", // 垂直方向に並べる
+        gap: "0px", // 要素間の間隔
+      }}
+    >
       <Header />
       <DataDisplayArea />
       <Footer />
