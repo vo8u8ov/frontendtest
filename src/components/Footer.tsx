9c25d145e68f6c321e0d06481c9b6d28fae29af2
mariko.s <>
2024-10-29 23:19:53 +0900

    フッターレイアウト修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 // src/Footer.tsx
 import React from "react";
 import FooterNav from "../footerNav.svg";
+import TermsIcon from "../terms.svg";
+import PrivacyPolicyIcon from "../privacypolicy.svg";
 import CopyrightLogo from "../copyright.svg";
 
 const Footer: React.FC = () => {
@@ -14,7 +16,37 @@ const Footer: React.FC = () => {
         borderTop: "1px solid #FFFFFF33",
       }}
     >
-      <div className="flex items-center">
+      <div className="flex items-center" style={{ gap: "24px" }}>
+        <div>
+          <div
+            className="flex items-center"
+            style={{
+              width: "auto",
+              height: "auto",
+              gap: "8px",
+            }}
+          >
+            <p
+              className="text-sm"
+              style={{
+                width: "48px",
+                height: "18px",
+                lineHeight: "18px",
+                fontFamily: "Noto Sans JP",
+                fontSize: "12px",
+                fontWeight: 400,
+                color: "#FFFFFF",
+              }}
+            >
+              利用規約
+            </p>
+            <img
+              src={TermsIcon}
+              alt="Terms Icon"
+              style={{ width: "12px", height: "12px" }}
+            />
+          </div>
+        </div>
         <div
           className="flex items-center"
           style={{
@@ -23,14 +55,41 @@ const Footer: React.FC = () => {
             gap: "24px",
           }}
         >
-          <img src={FooterNav} alt="Terms Icon" />
+          <div
+            className="flex items-center"
+            style={{
+              width: "auto",
+              height: "auto",
+              gap: "8px",
+            }}
+          >
+            <p
+              className="text-sm"
+              style={{
+                width: "120px",
+                height: "18px",
+                lineHeight: "18px",
+                fontFamily: "Noto Sans JP",
+                fontSize: "12px",
+                fontWeight: 400,
+                color: "#FFFFFF",
+              }}
+            >
+              プライバシーポリシー
+            </p>
+            <img
+              src={PrivacyPolicyIcon}
+              alt="Privacy Policy Icon"
+              style={{ width: "12px", height: "12px" }}
+            />
+          </div>
         </div>
       </div>
       <div className="flex items-center" style={{ gap: "24px" }}>
         <img
           src={CopyrightLogo}
           alt="Copyright"
-          style={{ width: "auto", height: "auto" }}
+          style={{ width: "101px", height: "18px" }}
         />
       </div>
     </footer>
